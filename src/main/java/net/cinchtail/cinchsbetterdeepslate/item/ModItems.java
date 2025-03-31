15f00d9b1b8bd4eba54f918522b16ed5e11f06af
Joshua Davies <cinchtail@gmail.com>
2025-03-31 16:52:02 -0400

    tool fixes

--- src/main/java/net/cinchtail/cinchsbetterdeepslate/item/ModItems.java
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/item/ModItems.java
@@ -13,34 +13,34 @@ import java.util.function.Function;
 public class ModItems {
 
     public static final Item BLACKSTONE_SWORD = registerItem("blackstone_sword",
-            properties -> new PickaxeItem(ModToolMaterials.BLACKSTONE, 3, -2.4F, new Item.Settings()
+            properties -> new SwordItem(ModToolMaterials.BLACKSTONE, 3, -2.4F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "blackstone_sword")))));
     public static final Item BLACKSTONE_PICKAXE = registerItem("blackstone_pickaxe",
             properties -> new PickaxeItem(ModToolMaterials.BLACKSTONE, 1.0F, -2.8F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "blackstone_pickaxe")))));
     public static final Item BLACKSTONE_AXE = registerItem("blackstone_axe",
-            properties -> new PickaxeItem(ModToolMaterials.BLACKSTONE, 7.0F, -3.2F, new Item.Settings()
+            properties -> new AxeItem(ModToolMaterials.BLACKSTONE, 7.0F, -3.2F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "blackstone_axe")))));
     public static final Item BLACKSTONE_SHOVEL = registerItem("blackstone_shovel",
-            properties -> new PickaxeItem(ModToolMaterials.BLACKSTONE, 1.5F, -3.0F, new Item.Settings()
+            properties -> new ShovelItem(ModToolMaterials.BLACKSTONE, 1.5F, -3.0F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "blackstone_shovel")))));
     public static final Item BLACKSTONE_HOE = registerItem("blackstone_hoe",
-            properties -> new PickaxeItem(ModToolMaterials.BLACKSTONE, -1.0F, -2.0F, new Item.Settings()
+            properties -> new HoeItem(ModToolMaterials.BLACKSTONE, -1.0F, -2.0F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "blackstone_hoe")))));
     public static final Item DEEPSLATE_SWORD = registerItem("deepslate_sword",
-            properties -> new PickaxeItem(ModToolMaterials.DEEPSLATE, 3, -2.4F, new Item.Settings()
+            properties -> new SwordItem(ModToolMaterials.DEEPSLATE, 3, -2.4F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "deepslate_sword")))));
     public static final Item DEEPSLATE_PICKAXE = registerItem("deepslate_pickaxe",
             properties -> new PickaxeItem(ModToolMaterials.DEEPSLATE, 1.0F, -2.8F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "deepslate_pickaxe")))));
     public static final Item DEEPSLATE_AXE = registerItem("deepslate_axe",
-            properties -> new PickaxeItem(ModToolMaterials.DEEPSLATE, 7.0F, -3.2F, new Item.Settings()
+            properties -> new AxeItem(ModToolMaterials.DEEPSLATE, 7.0F, -3.2F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "deepslate_axe")))));
     public static final Item DEEPSLATE_SHOVEL = registerItem("deepslate_shovel",
-            properties -> new PickaxeItem(ModToolMaterials.DEEPSLATE, 1.5F, -3.0F, new Item.Settings()
+            properties -> new ShovelItem(ModToolMaterials.DEEPSLATE, 1.5F, -3.0F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "deepslate_shovel")))));
     public static final Item DEEPSLATE_HOE = registerItem("deepslate_hoe",
-            properties -> new PickaxeItem(ModToolMaterials.DEEPSLATE, -1.0F, -2.0F, new Item.Settings()
+            properties -> new HoeItem(ModToolMaterials.DEEPSLATE, -1.0F, -2.0F, new Item.Settings()
                     .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CinchsBetterDeepslate.MOD_ID, "deepslate_hoe")))));
 
     public static <T extends Item> T registerItem(String name, Function<Item.Settings, T> factory) {
