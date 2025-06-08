677e3dd5d62955a5624056c6fe09039a9d0b612c
Joshua Davies <cinchtail@gmail.com>
2025-06-08 16:03:36 -0400

    fixes, tweaks, and new tags

--- src/main/java/net/cinchtail/cinchsbetterdeepslate/item/ModItems.java
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/item/ModItems.java
@@ -9,21 +9,6 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item BLACKSTONE_SWORD = registerItem("blackstone_sword",
-            new SwordItem(ModToolMaterial.BLACKSTONE,3, -2.4f,
-                    new FabricItemSettings()));
-    public static final Item BLACKSTONE_PICKAXE = registerItem("blackstone_pickaxe",
-            new PickaxeItem(ModToolMaterial.BLACKSTONE,1, -2.8F,
-                    new FabricItemSettings()));
-    public static final Item BLACKSTONE_AXE = registerItem("blackstone_axe",
-            new AxeItem(ModToolMaterial.BLACKSTONE,7F, -3.2F,
-                    new FabricItemSettings()));
-    public static final Item BLACKSTONE_SHOVEL = registerItem("blackstone_shovel",
-            new ShovelItem(ModToolMaterial.BLACKSTONE,1.5F, -3F,
-                    new FabricItemSettings()));
-    public static final Item BLACKSTONE_HOE = registerItem("blackstone_hoe",
-            new HoeItem(ModToolMaterial.BLACKSTONE,-1, -2F,
-                    new FabricItemSettings()));
     public static final Item DEEPSLATE_SWORD = registerItem("deepslate_sword",
             new SwordItem(ModToolMaterial.DEEPSLATE,3, -2.4F,
                     new FabricItemSettings()));
@@ -39,6 +24,23 @@ public class ModItems {
     public static final Item DEEPSLATE_HOE = registerItem("deepslate_hoe",
             new HoeItem(ModToolMaterial.DEEPSLATE,-1, -2F,
                     new FabricItemSettings()));
+
+    public static final Item BLACKSTONE_SWORD = registerItem("blackstone_sword",
+            new SwordItem(ModToolMaterial.BLACKSTONE,3, -2.4f,
+                    new FabricItemSettings()));
+    public static final Item BLACKSTONE_PICKAXE = registerItem("blackstone_pickaxe",
+            new PickaxeItem(ModToolMaterial.BLACKSTONE,1, -2.8F,
+                    new FabricItemSettings()));
+    public static final Item BLACKSTONE_AXE = registerItem("blackstone_axe",
+            new AxeItem(ModToolMaterial.BLACKSTONE,7F, -3.2F,
+                    new FabricItemSettings()));
+    public static final Item BLACKSTONE_SHOVEL = registerItem("blackstone_shovel",
+            new ShovelItem(ModToolMaterial.BLACKSTONE,1.5F, -3F,
+                    new FabricItemSettings()));
+    public static final Item BLACKSTONE_HOE = registerItem("blackstone_hoe",
+            new HoeItem(ModToolMaterial.BLACKSTONE,-1, -2F,
+                    new FabricItemSettings()));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(CinchsBetterDeepslate.MOD_ID, name), item);
     }
